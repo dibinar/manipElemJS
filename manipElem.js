7e1ce47d9938dc831ac34909aa15e19f023a09b6
FullaNeSushestvuet <113693077+dibinar@users.noreply.github.com>
2023-02-27 14:43:31 +0300

    Add files via upload

---
+++ manipElem.js
@@ -0,0 +1,51 @@
+//<324>
+// let p = document.createElement('p');
+// p.textContent = '!';
+
+// let elem = document.querySelector('#elem');
+// elem.insertAdjacentElement('beforeBegin', p);
+
+// let p = document.createElement('p');
+// p.textContent = '!';
+
+// let elem = document.querySelector('#elem');
+// elem.insertAdjacentElement('afterEnd', p);
+
+// let p = document.createElement('p');
+// p.textContent = '!';
+
+// let elem = document.querySelector('#elem');
+// elem.insertAdjacentElement('afterBegin', p);
+
+// let p = document.createElement('p');
+// p.textContent = '!';
+
+// let elem = document.querySelector('#elem');
+// elem.insertAdjacentElement('beforeEnd', p);
+
+// let elem = document.querySelector('#elem');
+// elem.insertAdjacentHTML('beforeBegin', '<div class="www"><p>text</p><p>text</p><input></div>');
+
+
+//<325>
+// let elem = document.querySelector("#elem")
+// let elem2 = elem.querySelector(".elem2")
+// let button = document.querySelector("#btn")
+// button.addEventListener('click',func)
+// function func(){
+//     let clone = elem2.cloneNode(true)
+//     elem.appendChild(clone)
+// }
+
+
+//<326>
+// let elem = document.querySelector('#elem');
+// console.log(elem.matches('div.www'));
+
+// let elem = document.querySelector('#elem');
+// console.log(elem.matches('p.www'));
+
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let a = elem1.contains(elem2)
+// console.log(a)
